aad150b197523b0affc77c740d2f79bb411161f0
VaraparlaNarasingarao <42230584+VaraparlaNarasingarao@users.noreply.github.com>
2020-04-03 17:16:31 +0530

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -31,12 +31,12 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
-<<<<<<< HEAD
+
 	public function paymet()
 	{
 		$this->load->view('welcome_message');
 	}
-=======
+
 	public function login()
 	{
 		$this->load->view('welcome_message');
@@ -46,5 +46,5 @@ class Welcome extends CI_Controller {
 		$this->load->view('welcome_message');
 	}
 
->>>>>>> master
+
 }
